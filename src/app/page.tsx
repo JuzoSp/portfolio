b2187d95140c95efbac5868ef161dc8af8fb277f
JuzoSp <speyerhasina@gmail.com>
2024-02-07 15:44:38 +0300

    mety

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/main/Hero";
 import Skills from "@/components/main/Skills";
 import "./globals.css";
+import Encryption from "@/components/main/Encryption";
 export default function Home() {
   return (
     <main className="h-full w-full ">
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
+        <Encryption />
       </div>
     </main>
   );
--- src/components/main/sub/SkillText.tsx
+++ src/components/main/sub/SkillText.tsx
@@ -12,7 +12,7 @@ const SkillText = () => {
                 className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
                 <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
                 <h1 className='Welcome-text text-[13px]'>
-                    with Next js
+                    With javascript and his library
                 </h1>
             </motion.div>
             <motion.div
