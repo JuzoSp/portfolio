cf86be7fa8436caa1ca3b98ecd589cf6a4f13fe7
JuzoSp <speyerhasina@gmail.com>
2024-02-08 14:36:37 +0300

    ok2

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'>
             <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
-                <a href="about-me" className='h-auto w-auto flex flex-row items-center'>
+                <a href="#about-me" className='h-auto w-auto flex flex-row items-center'>
                     <div className='w-[54px] h-[54px] rounded-full flex justify-center items-center bg-gradient-to-tr from-purple-400 to-cyan-200'>
                         <Image
                             src="/Image.jpg"
@@ -16,17 +16,15 @@ const Navbar = () => {
                             className='cursor-pointer hover:animate-slowspin rounded-full'
                         />
                     </div>
-
                     <span className='font-bold ml-[10px] hidden md:block text-gray-300'>
                         Hasina
                     </span>
                 </a>
-
-                <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
-                    <div className='flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
-                        <a href="#about-me" className='cursor-pointer p-[5px] hover:bg-gradient-to-tr from-purple-600 to-cyan-500 hover:opacity-50 rounded-full'>À propos</a>
-                        <a href="#skills" className='cursor-pointer p-[5px] hover:bg-gradient-to-tr from-purple-600 to-cyan-500 hover:opacity-50 rounded-full'>Compétences</a>
-                        <a href="#projects" className='cursor-pointer p-[5px] hover:bg-gradient-to-tr from-purple-600 to-cyan-500 hover:opacity-50 rounded-full'>Projets</a>
+                <div className='w-[640px] h-full flex flex-row items-center gap-2 justify-between md:mr-10'>
+                    <div className='flex items-center justify-between w-full h-[54px] border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
+                        <a href="#about-me" className='cursor-pointer hover:bg-gradient-to-tr from-purple-600 to-cyan-500 hover:opacity-50 rounded-full p-2'>About</a>
+                        <a href="#skills" className='cursor-pointer hover:bg-gradient-to-tr from-purple-600 to-cyan-500 hover:opacity-50 rounded-full p-2'>Compétences</a>
+                        <a href="#projects" className='cursor-pointer hover:bg-gradient-to-tr from-purple-600 to-cyan-500 hover:opacity-50 rounded-full p-2'>Projets</a>
                     </div>
                 </div>
 
--- src/components/main/sub/HeroContent.tsx
+++ src/components/main/sub/HeroContent.tsx
@@ -15,7 +15,7 @@ const HeroContent = () => {
             <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
                 <motion.div
                     variants={slideInFromTop}
-                    className='Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
+                    className='Welcome-box py-[7px] px-[7px] border border-[#7042f88b] opacity-[0.9]'>
                     <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
                     <h1 className='Welcome-text text-[13px]'>
                         Front-end Developer
@@ -23,7 +23,7 @@ const HeroContent = () => {
                 </motion.div>
                 <motion.div
                     variants={slideInFromLeft(0.5)}
-                    className='flex flex-col gap-6 mt-6 text-5xl md:text-md sm:text-md font-bold text-white max-w-[600px] w-auto h-auto'
+                    className='flex flex-col gap-6 mt-[54px] text-5xl md:text-md sm:text-md font-bold text-white max-w-[600px] w-auto h-auto'
                 >
                     <span>
                         Offrir
